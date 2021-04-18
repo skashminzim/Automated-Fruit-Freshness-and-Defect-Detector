bd9d29f57e93327848ecea1598d82646720e039a
zannat207 <54244858+zannat207@users.noreply.github.com>
2021-04-19 01:02:57 +0600

    Update stats.php

--- Frontend/stats.php
+++ Frontend/stats.php
@@ -157,12 +157,12 @@ table, th, td {
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"><a href="index.php" class="nav-link"><b>Home</b></a></li>
+	          <li class="nav-item"><a href="index.php" class="nav-link"><b>Home</b></a></li>
 
 
 
 	          <li class="nav-item"><a href="image.php" class="nav-link"><b>Image Processing</b></a></li>
-			        <li class="nav-item"><a href="stats.php" class="nav-link"><b>Statistics</b></a></li>
+			        <li class="nav-item active"><a href="stats.php" class="nav-link"><b>Statistics</b></a></li>
 			            <li class="nav-item"><a href="about.php" class="nav-link"><b>About</b></a></li>
 			  <!-- <li class="nav-item"><a href="signin.php" class="nav-link"><b>Sign in</b></a></li>-->
         <li class="nav-item"><a href="logout.php" class="nav-link"><b>Log Out</b></a></li>
